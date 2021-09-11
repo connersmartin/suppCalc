21167392e4ce77cbc8296e01d46908f54c0b988b
Conner Martin <connersmartin@gmail.com>
2021-09-10 20:20:37 -0700

    added share functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
+import React from 'react';
 
-const App = () => {
-  return (
-    <div className="App">
-        <h1>Hey, this calculates your daily expenditure on supplements</h1>    
-    </div>
-  );
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hey, this calculates your daily expenditure on supplements</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Share.js
+++ src/Share.js
@@ -1,21 +1,33 @@
 import React from 'react';
+import { v4 } from 'uuid';
 
 class Share extends React.Component {
     constructor(props) {
         super(props);
-
+        this.state = {
+            shareId: ''
+        }
         this.handleSubmit = this.handleSubmit.bind(this);
 
 
     }
     handleSubmit() {
         console.log("Share clicked!");
+        let shareObj = {};
+        shareObj.rows = this.props.rows
+        shareObj.uid = v4();
+        this.setState({shareId: shareObj.uid});
+        let data = JSON.stringify(shareObj);
+        console.log(data);
+        let share = document.getElementById('shareId');
+        share.innerText = this.state.shareId;
     }
 
     render() {
         return (
             <div>
                 <button className="btn btn-primary" onClick={this.handleSubmit}>Share</button>
+                <span id="shareId"></span>
             </div>
         )
     }
